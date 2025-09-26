a8f642efa7d3d94f6eda22efae6d6f4548aefc90
unknown <you@example.com>
2025-09-26 12:06:39 -0700

    diploy dist

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
 import './index.css'
 import App from './App.jsx'
+import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
