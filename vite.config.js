9017876ad6284d5a7b676555d8e2289ca3ca7e0d
unknown <you@example.com>
2025-09-26 12:55:52 -0700

    add dist

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,17 @@
 //    base: '/customer-support-zone/',
 //   plugins: [react(),tailwindcss()],
 // })
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   base: '/customer-support-zone/', // GitHub Pages repo name
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/customer-support-zone/', // GitHub Pages repo name
+  base: '/customer-support-zone/', // repo-name
   plugins: [react()],
 })
